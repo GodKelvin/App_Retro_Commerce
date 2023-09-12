8830e35f71e9a7088e61f0c23275d0cdfb8fb343
GodKelvin <kelvin.lehrback@gmail.com>
2023-09-12 16:33:55 -0300

    implementando tela de login e autenticacao

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController, ToastController } from '@ionic/angular';
+import { Login } from 'src/app/interfaces/login';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,19 +9,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  dataLogin = {
+  dataLogin: Login =  {
     email: "",
     senha: ""
   }
-  constructor() { }
+  constructor(
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController,
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
   }
 
-  login(){
-    console.log(this.dataLogin.email, this.dataLogin.senha);
-    console.log(this.dataLogin)
+  async login(){
+
+    //Transformar o load num service?
+    const loading = await this.loadingCtrl.create({
+      message: "Carregando...",
+      spinner: 'bubbles'
+    });
+    await loading.present();
+    this.authService.login(this.dataLogin).subscribe({
+        next: (value) => {
+          console.log("Sucesso", value);
+        },
+        error: async (error) => {
+          const toast = await this.toastCtrl.create({
+            message: error.error.message,
+            position: "top",
+            duration: 5000,
+            color: "danger"
+          });
+      
+          toast.present();
+        }
+    })
+    loading.dismiss();
+
+    
   }
 
 }
