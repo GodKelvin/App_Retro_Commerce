75a1e6ff366362dd4f315d1a3ea4fdc4e9f5c85a
GodKelvin <kelvin.lehrback@gmail.com>
2023-09-14 18:50:10 -0300

    wip: Tela inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -14,6 +15,11 @@ const routes: Routes = [
   {
     path: 'cadastro',
     loadChildren: () => import('./pages/cadastro/cadastro.module').then( m => m.CadastroPageModule)
+  },
+  {
+    path: 'tela-inicial',
+    loadChildren: () => import('./pages/tela-inicial/tela-inicial.module').then( m => m.TelaInicialPageModule),
+    canActivate: [AuthGuard]
   },
 ];
 
