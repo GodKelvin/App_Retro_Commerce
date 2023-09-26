b1b1b3d3333cafef093f7750065bc9e4fac695b9
GodKelvin <kelvin.lehrback@gmail.com>
2023-09-25 21:05:48 -0300

    removendo importacao desnecessario

--- src/app/services/anuncio.service.ts
+++ src/app/services/anuncio.service.ts
@@ -2,7 +2,6 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Anuncio } from '../interfaces/anuncio';
 
 @Injectable({
   providedIn: 'root'
