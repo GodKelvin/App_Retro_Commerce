e89532c52e5619e7049ec6d8c4c5975a40394874
GodKelvin <kelvin.lehrback@gmail.com>
2023-10-16 19:43:27 -0300

    alterando layout de busca de anuncios

--- src/app/pages/main-tabs/home/home.page.ts
+++ src/app/pages/main-tabs/home/home.page.ts
@@ -55,4 +55,15 @@ export class HomePage implements OnInit {
     return `${ano}-${mes}-${dia}`;
   }
 
+  formataData(data: string | undefined){  
+    if(data){
+      const date = new Date(data);
+      const dia = String(date.getDate()).padStart(2, '0');
+      const mes = String(date.getMonth() + 1).padStart(2, '0');
+      const ano = date.getFullYear();
+      return `${dia}/${mes}/${ano}`;
+    }
+    return null; 
+  }
+
 }
