65ec0cce2eed9aaa97ff90d95972a03b4b1c6e07
GodKelvin <kelvin.lehrback@gmail.com>
2023-10-16 18:10:24 -0300

    melhorando layout de meus itens

--- src/app/pages/main-tabs/meus-anuncios/meus-anuncios.page.ts
+++ src/app/pages/main-tabs/meus-anuncios/meus-anuncios.page.ts
@@ -38,4 +38,15 @@ export class MeusAnunciosPage implements OnInit {
     if(event) event.target.complete();
   }
 
+  formataData(data: string | undefined){  
+    if(data){
+      const date = new Date(data);
+      const dia = String(date.getDate()).padStart(2, '0');
+      const mes = String(date.getMonth() + 1).padStart(2, '0');
+      const ano = date.getFullYear();
+      return `${dia}/${mes}/${ano}`;
+    }
+    return null; 
+  }
+
 }
